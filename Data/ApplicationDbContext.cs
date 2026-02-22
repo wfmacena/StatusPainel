2bb94afb0053c2c4aada316fd3a159aeb0c7dfb9
Anonimos2024 <willzona@hotmail.com>
2026-02-21 23:06:01 -0300

    Força inclusão da pasta Data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StatusPainel.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Se você tiver outras tabelas, adicione aqui
+        // public DbSet<GameStatus> GameStatuses { get; set; }
+    }
+}
